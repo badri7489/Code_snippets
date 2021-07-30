1b1339af0ea2fb08c02dce2f8db96b80cdfc92b7
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-07-31 00:04:27 +0530

    Matrix Median Updated

--- Binary Search/02_matrix_median_of_sorted_array.cpp
+++ Binary Search/02_matrix_median_of_sorted_array.cpp
@@ -1,3 +1,14 @@
+/** Matrix median is finding the median of the matrix values if placed in a sorted order.
+  * We suppose that the median lies in range 1-10^9 (Taken as such beacuse of the constraints).
+  * Binary search is applied and each time we calculate the mid value,
+  * then we count the number of values in the matrix which are less than or equal to mid(using binary search).
+  * If the count is less than or equal to half of the number of elements, then make low = mid+1 else high = mid-1.
+  * Low value is the required median.
+  * In order to calculate the number of elements in the matrix which are less than or equal to mid,
+  * for every row of the matrix run a binary search to find the index of the element greater than mid.
+  * Low value gives the count of the number of elements less than or equal to mid.
+  */
+
 #include<bits/stdc++.h>
 using namespace std;
 

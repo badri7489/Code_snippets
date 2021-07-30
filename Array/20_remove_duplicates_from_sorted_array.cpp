1dfc20bf75344c6da264a7590a01c86ffcfe43ac
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-07-30 22:33:54 +0530

    Array Problems updated

---
+++ Array/20_remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,20 @@
+/** Brute force is to take an array and while traversing the given array we will store all the unique one
+  * and then return the size of the new array.
+  * Optimal solution without extra space would be to use two pointers.
+  * One pointer will traverse the whole array and the other will only be updated if we find a new value.
+  * Now we return the value of the second pointer + 1.
+  * One edge condition would be there when the size of the array is zero.
+  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    int i = 0, n = nums.size(), k = 1;
+    while (k < n) {
+        if (nums[i] != nums[k])
+            nums[++i] = nums[k];
+        k++;
+    }
+    return (n) ? i + 1 : 0;
+}
---
+++ Array/21_maximum_consecutive_ones.cpp
@@ -0,0 +1,21 @@
+/** Only approach is to traverse the whole array and count the number of ones
+  * and if we encounter a 0 we store the counter in the max and make the counter zero.
+  * Hence we will have the max number of ones.
+  * Time will be O(n) and space will be constant.
+  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int n = nums.size(), maxi = 0, k = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == 1) {
+            k++;
+        } else {
+            maxi = max(maxi, k);
+            k = 0;
+        }
+    }
+    return max(maxi, k);
+}

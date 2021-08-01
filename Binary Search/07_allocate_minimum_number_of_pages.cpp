48e3d24726dd9635c7817dd1fccb4d9bded97dcb
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-08-01 23:28:56 +0530

    Allocate minimum number of pages

---
+++ Binary Search/07_allocate_minimum_number_of_pages.cpp
@@ -0,0 +1,43 @@
+/**
+ *
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool possible(int arr[], int n, int m, int barrier) {
+    int stud = 0, pages = 0;
+    for (int i = 0; i < n; i++) {
+        if (pages + arr[i] > barrier) {
+            stud++;
+            pages = arr[i];
+            if (pages > barrier)
+                return false;
+        } else {
+            pages += arr[i];
+        }
+    }
+    if (stud < m)
+        return true;
+    return false;
+}
+
+int findPages(int arr[], int n, int m) {
+    if (m > n)
+        return -1;
+    int low = arr[0], high = 0, ans = -1;
+    for (int i = 0; i < n; i++) {
+        high += arr[i];
+        low = min(low, arr[i]);
+    }
+    while (low <= high) {
+        int mid = (low + high) >> 1;
+        if (possible(arr, n, m, mid)) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}

da0d02b9d25c1d30d7eb0ed4b67d3949246a1a98
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-08-13 18:17:38 +0530

    Postorder Traversal using recursion and iteration

---
+++ Tree/03_postorder_traversal.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// Recursive Code
+void postOrder(TreeNode* root, vector<int>& ans) {
+    if (!root)
+        return;
+    postOrder(root->left, ans);
+    postOrder(root->right, ans);
+    ans.push_back(root->val);
+}
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    postOrder(root, ans);
+    return ans;
+}
+
+// Iterative Code
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    stack<TreeNode*> stk;
+    TreeNode* temp = root;
+    while (temp || !stk.empty()) {
+        if (temp) {
+            stk.push(temp);
+            temp = temp->left;
+        } else {
+            TreeNode* node = stk.top()->right;
+            if (node) {
+                temp = node;
+            } else {
+                node = stk.top();
+                stk.pop();
+                ans.push_back(node->val);
+                while (!stk.empty() && node == stk.top()->right) {
+                    node = stk.top();
+                    stk.pop();
+                    ans.push_back(node->val);
+                }
+            }
+        }
+    }
+    return ans;
+}

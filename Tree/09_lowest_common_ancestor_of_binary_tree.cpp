26e16e7bd45c60a3ce891028753a76f09ac3a80c
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-08-14 11:22:23 +0530

    LCA in Binary Tree

---
+++ Tree/09_lowest_common_ancestor_of_binary_tree.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root)
+        return root;
+    if (p == root || q == root)
+        return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    if (left && right)
+        return root;
+    if (!left && !right)
+        return NULL;
+    return left ? left : right;
+}

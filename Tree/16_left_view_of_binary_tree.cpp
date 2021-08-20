ac21c19cbd2e24ae6b5bcc76bc7ca992c2939c57
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-08-20 13:01:28 +0530

    Left View of Binary Tree

---
+++ Tree/16_left_view_of_binary_tree.cpp
@@ -0,0 +1,68 @@
+/**
+  * We just do Level order traversal and keep the take the first element of each level.
+  * All the first element at each element is the Left View of the Binary Tree.
+  * Time Complexity: O(n).
+  * Space Complexity: O(n).
+  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// Using q.size() to check the level.
+vector<int> leftView(TreeNode* root) {
+    if (!root)
+        return {};
+    vector<int> ans;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        int n = q.size();
+        for (int i = 0; i < n; i++) {
+            TreeNode* p = q.front();
+            q.pop();
+            if (i == 0)
+                ans.push_back(p->val);
+            if (p->left)
+                q.push(p->left);
+            if (p->right)
+                q.push(p->right);
+        }
+    }
+    return ans;
+}
+
+// Using NULL as a flag to change level.
+vector<int> leftView(TreeNode* root) {
+    if (!root)
+        return {};
+    vector<int> ans;
+    queue<TreeNode*> q;
+    q.push(root);
+    q.push(NULL);
+    ans.push_back(root->val);
+    while (!q.empty()) {
+        TreeNode* temp = q.front();
+        q.pop();
+        if (temp) {
+            if (temp->left)
+                q.push(temp->left);
+            if (temp->right)
+                q.push(temp->right);
+        } else if (q.front() && !q.empty()) {
+            q.push(NULL);
+            ans.push_back(q.front()->val);
+        }
+        if (!temp && !q.front())
+            break;
+    }
+    return ans;
+}

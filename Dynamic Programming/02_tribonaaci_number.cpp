0f95a00a80fb4e652e8475360d568000dc99a6d2
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-08-29 01:15:34 +0530

    Tribonaaci Numbers

---
+++ Dynamic Programming/02_tribonaaci_number.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int tribonacci(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1 || n == 2)
+        return 1;
+    vector<int> dp(n + 1, 0);
+    dp[1] = dp[2] = 1;
+    for (int i = 3; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    return dp[n];
+}

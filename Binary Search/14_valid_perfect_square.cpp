7343811062410e689089029fc59809b0e739cd21
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-09-01 17:38:39 +0530

    Valid Perfect Square

---
+++ Binary Search/14_valid_perfect_square.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPerfectSquare(int num) {
+    int l = 1, h = num, m, x;
+    while (l <= h) {
+        m = l + (h - l) / 2;
+        x = num / m;
+        if (x == m && num % m == 0)
+            return true;
+        else if (x < m)
+            h = m - 1;
+        else
+            l = m + 1;
+    }
+    return false;
+}

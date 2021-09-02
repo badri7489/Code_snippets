52c089f284b9fe17dbeb118f570a6a12789396d2
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-09-02 19:00:00 +0530

    DP Code Edited

--- Array/17_grid_unique_paths.cpp
+++ Array/17_grid_unique_paths.cpp
@@ -29,14 +29,10 @@ int uniquePaths(int m, int n) {
 
 // DP code
 int uniquePaths(int m, int n) {
-    int dp[m][n];
-    for (int i = m - 1;i >= 0;i--) {
-        for (int j = n - 1; j >= 0; j--) {
-            if (i == m - 1 || j == n - 1)
-                dp[i][j] = 1;
-            else
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-        }
+    vector<vector<int>> dp(m, vector<int>(n, 1));
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++)
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
     }
-    return dp[0][0];
+    return dp[m - 1][n - 1];
 }

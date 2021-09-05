79bf5c68da20b824c908e4007dddfc7bc98326a4
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-09-05 16:24:07 +0530

    Comments Updated

--- Array/01_sort_array_containing_0_1_2_only.cpp
+++ Array/01_sort_array_containing_0_1_2_only.cpp
@@ -4,7 +4,9 @@
   * Then we can loop the array and write all the values accordingly.
   * But if we want to do inplace sort without preprocessing
   * then we can sort by pointing the values of each one of them.
-  * The variable low is for pointing 0’s, mid is for pointing at 1’s and high is for pointing 2’s.
+  * The low pointer points the index before which all are 0's.
+  * The high pointer points the position after which all are 2's
+  * The loop stops when mid crosses high.
   */
 
 #include <bits/stdc++.h>

0caffe6ddacea5529cccfd2bf830a02801e4bc70
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-09-08 00:19:11 +0530

    Jump Game

---
+++ Dynamic Programming/14_jump_game.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Recursive Approach
+bool helper(vector<int>& arr, int i) {
+    if (i >= arr.size())
+        return false;
+    if (i == arr.size() - 1)
+        return true;
+    for (int j = 1; j <= arr[i]; j++) {
+        if (helper(arr, i + j))
+            return true;
+    }
+    return false;
+}
+
+bool canJump(vector<int>& nums) {
+    return helper(nums, 0);
+}
+
+// DP Approach
+bool canJump(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 1)
+        return true;
+    vector<bool> dp(n, false);
+    dp[n - 1] = true;
+    for (int i = n - 2; i >= 0; i--) {
+        if (nums[i] != 0) {
+            for (int j = 1; j <= nums[i]; j++) {
+                dp[i] = dp[i] || dp[i + j];
+                if (dp[i])
+                    break;
+            }
+        }
+    }
+    return dp[0];
+}

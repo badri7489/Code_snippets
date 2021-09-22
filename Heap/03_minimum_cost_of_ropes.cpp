fa9b4f8ee668b51f48938cdbca43cb4edd6257f8
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-09-22 17:09:08 +0530

    Minimum Cost to join all the ropes

---
+++ Heap/03_minimum_cost_of_ropes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<queue>
+#define ll long long
+using namespace std;
+
+ll minCost(ll arr[], ll n) {
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    for (ll i = 0; i < n; i++)
+        pq.push(arr[i]);
+    ll cost = 0, min1, min2;
+    while (pq.size() > 1) {
+        min1 = pq.top();
+        pq.pop();
+        min2 = pq.top();
+        pq.pop();
+        cost += min1 + min2;
+        pq.push(min1 + min2);
+    }
+    return cost;
+}

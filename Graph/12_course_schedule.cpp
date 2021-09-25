f0ef8ebdad9802365988501f646181647604947f
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-09-25 20:08:31 +0530

    Course Schedule Topo Sort

--- Graph/12_course_schedule.cpp
+++ Graph/12_course_schedule.cpp
@@ -5,20 +5,18 @@ using namespace std;
 
 class Solution {
 private:
-    void createGraph(vector<vector<int>>& arr, vector<int> adj[]) {
-        for (int i = 0; i < arr.size(); i++)
-            adj[arr[i][0]].push_back(arr[i][1]);
+    void createGraph(vector<vector<int>>& graph, vector<int> adj[], vector<int>& inDeg) {
+        for (int i = 0; i < graph.size(); i++) {
+            adj[graph[i][1]].push_back(graph[i][0]);
+            inDeg[graph[i][0]]++;
+        }
     }
 public:
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+    bool canFinish(int n, vector<vector<int>>& graph) {
         vector<int> adj[n];
-        createGraph(prerequisites, adj);
-        queue<int> q;
         vector<int> inDeg(n, 0);
-        for (int i = 0; i < n; i++) {
-            for (int j : adj[i])
-                inDeg[j]++;
-        }
+        createGraph(graph, adj, inDeg);
+        queue<int> q;
         for (int i = 0; i < n; i++) {
             if (inDeg[i] == 0)
                 q.push(i);
@@ -30,7 +28,7 @@ public:
             ans.push_back(node);
             for (int i : adj[node]) {
                 inDeg[i]--;
-                if (!inDeg[i])
+                if (inDeg[i] == 0)
                     q.push(i);
             }
         }

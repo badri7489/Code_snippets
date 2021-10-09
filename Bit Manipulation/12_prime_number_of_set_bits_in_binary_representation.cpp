89a7c10a8896af10086a71b6a337dfac00059014
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-10-09 12:11:10 +0530

    Prime number of set bits

---
+++ Bit Manipulation/12_prime_number_of_set_bits_in_binary_representation.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+class Solution {
+private:
+    unordered_set<int> prime;
+    bool setBit(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n % 2;
+            n /= 2;
+        }
+        return prime.find(count) != prime.end();
+    }
+public:
+    int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        prime = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+        for (int i = left; i <= right; i++) {
+            if (setBit(i))
+                count++;
+        }
+        return count;
+    }
+};

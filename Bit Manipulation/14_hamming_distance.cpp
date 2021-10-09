a6282c096f2dcc56a42a37ca66d4d68ab0efaeda
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-10-09 23:15:47 +0530

    Hamming Distance

---
+++ Bit Manipulation/14_hamming_distance.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans = 0;
+        while (x > 0 || y > 0) {
+            ans += (x % 2) ^ (y % 2);
+            x /= 2;
+            y /= 2;
+        }
+        return ans;
+    }
+};

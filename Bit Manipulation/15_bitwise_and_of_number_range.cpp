04070c11256942168b6807e0e75ffe0c445f93db
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-10-10 22:57:46 +0530

    Bitwise and of a range of numbers.

---
+++ Bit Manipulation/15_bitwise_and_of_number_range.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
+        while (left != right) {
+            left = left >> 1;
+            right = right >> 1;
+            count++;
+        }
+        return left << count;
+    }
+};

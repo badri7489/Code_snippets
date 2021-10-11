bdd46a370cfc2c80cf2b0dfc2ad7d5db94c73075
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-10-11 13:29:05 +0530

    Diameter of binary tree explaination

--- Tree/19_diameter_of_binary_tree.cpp
+++ Tree/19_diameter_of_binary_tree.cpp
@@ -1,3 +1,12 @@
+/**
+  * So although the longest path doesn't have to go through the root node,
+  * it has to pass the root node of some subtree of the tree (because it has to be from one leaf node to another leaf node,
+  * otherwise we can extend it for free).
+  * The longest path that passes a given node as the ROOT node is
+  * T = left_height + right_height.
+  * So you just calculate T for all nodes and output the max T.
+  */
+
 #include<bits/stdc++.h>
 using namespace std;
 

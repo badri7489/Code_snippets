dccd7750cd8c51d09bd5089371b4c45715fc2823
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-10-13 15:10:39 +0530

    Invert a Binary Tree

---
+++ Tree/36_invert_binary_tree.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root)
+            return root;
+        TreeNode* temp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(temp);
+        return root;
+    }
+};

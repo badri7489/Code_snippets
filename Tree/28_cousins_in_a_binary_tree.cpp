741939b090b78eb1008cb30a66279382d5add0e3
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-10-19 01:20:21 +0530

    Comments added

--- Tree/28_cousins_in_a_binary_tree.cpp
+++ Tree/28_cousins_in_a_binary_tree.cpp
@@ -17,25 +17,25 @@ bool isCousins(TreeNode* root, int x, int y) {
     pair<TreeNode*, TreeNode*> node;
     TreeNode* parentX = NULL, * parentY = NULL;
     int levelSize = 0;
-    while (!q.empty()) {
+    while (!q.empty()) {    // Doing a level order traversal.
         levelSize = q.size();
         while (levelSize--) {
             node = q.front();
             q.pop();
-            if (node.first->val == x)
+            if (node.first->val == x)   // If we find x, we store its parent.
                 parentX = node.second;
-            if (node.first->val == y)
+            if (node.first->val == y)   // If we get y, we store its parent.
                 parentY = node.second;
-            if (node.first->left)
+            if (node.first->left)   // Push left of the node.
                 q.push(make_pair(node.first->left, node.first));
-            if (node.first->right)
+            if (node.first->right)  // Push right of the node.
                 q.push(make_pair(node.first->right, node.first));
-            if (parentX && parentY)
+            if (parentX && parentY) // If we find both the parents, we immediately break.
                 break;
         }
-        if (parentX && parentY)
+        if (parentX && parentY) // If we find both the parents at this level, we check if they are same or not.
             return parentX != parentY;
-        if ((parentX && !parentY) || (!parentX && parentY))
+        if ((parentX && !parentY) || (!parentX && parentY)) // If we find only one of them, they are not cousins.
             return false;
     }
     return false;

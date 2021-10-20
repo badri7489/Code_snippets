fb3eb7f5f6d0a14c07c34e9f43a67c0f517560a0
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-10-21 00:48:19 +0530

    Comments Added, redundant checking removed

--- Dynamic Programming/07_longest_palindromic_substring.cpp
+++ Dynamic Programming/07_longest_palindromic_substring.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 
 // DP
@@ -6,21 +7,20 @@ string longestPalindrome(string s) {
     if (s.length() == 1)
         return s;
     int n = s.length(), i = 0, j = 0, maxi = 1, end;
-    int dp[n][n];
-    memset(dp, 0, sizeof(dp));
-    for (int k = 0; k < n; k++)
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    for (int k = 0; k < n; k++) // All are palindromes in itself.
         dp[k][k] = 1;
-    for (int k = 0; k < n - 1; k++) {
+    for (int k = 0; k < n - 1; k++) {   // Two consecutive characters are palindrome or not.
         if (s[k] == s[k + 1])
             dp[k][k + 1] = 1, i = k, j = k + 1, maxi = 2;
     }
-    for (int k = 2; k <= n; k++) {
-        for (int start = 0; start < n - k + 1; start++) {
-            end = start + k - 1;
+    for (int k = 3; k <= n; k++) {  // k denotes the size of each ssubstring.
+        for (int start = 0; start < n - k + 1; start++) {   // Denotes the starting point.
+            end = start + k - 1;    // Denotes the ending point.
             cout << "Compairing: " << s.substr(start, end - start + 1) << endl;
-            if (s[start] == s[end] && dp[start + 1][end - 1]) {
+            if (s[start] == s[end] && dp[start + 1][end - 1]) { // If the ends are equal then we check for the remaining in between ones.
                 dp[start][end] = 1;
-                if (maxi < end - start + 1)
+                if (maxi < end - start + 1) // Keeping the maximum one.
                     i = start, j = end, maxi = end - start + 1;
             }
         }

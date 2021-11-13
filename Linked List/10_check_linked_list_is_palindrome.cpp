0548935eb49d51d194d3227d909a2ed0ac66aeea
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-11-13 11:29:58 +0530

    Modified Code

--- Linked List/10_check_linked_list_is_palindrome.cpp
+++ Linked List/10_check_linked_list_is_palindrome.cpp
@@ -9,32 +9,38 @@ struct ListNode {
     ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
-bool isPalindrome(ListNode* head) {
-    if (!head || !head->next)
-        return true;
-    ListNode* slow = head, * fast = head;
-    while (fast->next && fast->next->next) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    if (slow->next->next) {
-        ListNode* prev = slow->next, * curr = prev->next, * temp = curr->next;
-        prev->next = NULL;
-        while (temp) {
+class Solution {
+private:
+    ListNode* rev(ListNode* head) {
+        ListNode* prev = NULL, * curr = head;
+        while (curr) {
+            head = head->next;
             curr->next = prev;
             prev = curr;
-            curr = temp;
-            temp = temp->next;
+            curr = head;
         }
-        curr->next = prev;
-        slow->next = curr;
+        return prev;
     }
-    ListNode* ptr1 = head, * ptr2 = slow->next;
-    while (ptr2) {
-        if (ptr1->val != ptr2->val)
-            return false;
-        ptr1 = ptr1->next;
-        ptr2 = ptr2->next;
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next)
+            return true;
+        ListNode* slow = head, * fast = head;
+        // Finding the mid of the linked list.
+        // The lesser of the two mids for even length linked list.
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        slow->next = rev(slow->next);   // Reversing the last side of the linked list
+        slow = slow->next;
+        fast = head;
+        while (slow) {
+            if (slow->val != fast->val) // Checking if both the side of the linked list are equal or not.
+                return false;
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return true;
     }
-    return true;
-}
+};

b520a8b062a491573be459c799277cc822967371
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-11-14 17:42:25 +0530

    Comments Added

--- Stack and Queue/10_sliding_window_maximum.cpp
+++ Stack and Queue/10_sliding_window_maximum.cpp
@@ -27,12 +27,12 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
     deque<int> dq;
     vector<int> ans;
     for (int i = 0; i < nums.size(); i++) {
-        if (!dq.empty() && dq.front() == i - k)
+        if (!dq.empty() && dq.front() == i - k) // Out of bound for the window of size 'k'
             dq.pop_front();
-        while (!dq.empty() && nums[dq.back()] < nums[i])
+        while (!dq.empty() && nums[dq.back()] < nums[i])    // remove all elements < a[i]
             dq.pop_back();
-        dq.push_back(i);
-        if (i >= k - 1)
+        dq.push_back(i);    // Take the current element
+        if (i >= k - 1) // Start taking elements after i >= k - 1
             ans.push_back(nums[dq.front()]);
     }
     return ans;

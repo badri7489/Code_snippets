6f671ca950312c3f67c0d3fb084e0be447dde2a7
badri7489 <59701113+badri7489@users.noreply.github.com>
2021-11-14 18:04:04 +0530

    Easier Code with explaination

--- Stack and Queue/11_asteroid_collision.cpp
+++ Stack and Queue/11_asteroid_collision.cpp
@@ -53,3 +53,41 @@ vector<int> asteroidCollision(vector<int>& arr) {
     }
     return ans;
 }
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& arr) {
+        stack<int> stk;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            if (stk.empty() || arr[i] > 0)  // If element is positive we simply add it to the stack.
+                stk.push(arr[i]);
+            else {
+                while (true) {
+                    int top = stk.top();
+                    if (top < 0) {  // If top is also negative we simply push the element and break.
+                        stk.push(arr[i]);
+                        break;
+                    } else if (top == -arr[i]) {    // If both are of same size, we pop and break.
+                        stk.pop();
+                        break;
+                    } else if (top > -arr[i]) {     // If top is more then the present one breaks.
+                        break;
+                    } else {    // If the present asteroid is bigger then it breaks the present and also the rest.
+                        stk.pop();
+                        if (stk.empty()) {  // If no more asteroids left, we take this asteroid and break.
+                            stk.push(arr[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        vector<int> ans(stk.size());
+        for (int i = stk.size() - 1; i >= 0; i--) {
+            ans[i] = stk.top();
+            stk.pop();
+        }
+        return ans;
+    }
+};

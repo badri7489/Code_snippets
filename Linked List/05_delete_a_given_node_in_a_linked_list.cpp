bca54e6f031eefcc59b8028713266b1fc8e876df
Biswanath Prasad Sharma <badrisharma333@gmail.com>
2022-11-21 23:27:27 +0530

    Indentation

--- Linked List/05_delete_a_given_node_in_a_linked_list.cpp
+++ Linked List/05_delete_a_given_node_in_a_linked_list.cpp
@@ -9,16 +9,16 @@
 using namespace std;
 
 struct ListNode {
-  int val;
-  ListNode* next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode* next) : val(x), next(next) {}
+  	int val;
+  	ListNode* next;
+  	ListNode() : val(0), next(nullptr) {}
+  	ListNode(int x) : val(x), next(nullptr) {}
+  	ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 void deleteNode(ListNode* node) {
-  node->val = node->next->val;
-  ListNode* temp = node->next;
-  node->next = temp->next;
-  delete temp;
+  	node->val = node->next->val;
+  	ListNode* temp = node->next;
+  	node->next = temp->next;
+  	delete temp;
 }
